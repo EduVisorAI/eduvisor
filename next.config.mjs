a0af29978e9bf5abca235de6a7ca77ff71864ec3
Mathyu-Serverli <Mathyu@serverli.com>
2024-08-30 12:53:45 -0500

    fix: update nextjs and fix docker problem with unicode

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,20 @@ const nextConfig = {
       },
     ],
   },
+  swcMinify: false, // Desactiva la minificación para evitar el error de Terser
+  webpack(config) {
+    if (config.optimization.minimizer) {
+      config.optimization.minimizer.forEach((minimizer) => {
+        if (minimizer.options && minimizer.options.terserOptions) {
+          minimizer.options.terserOptions.output = {
+            ...minimizer.options.terserOptions.output,
+            ascii_only: true, // Solo caracteres ASCII para evitar errores de unicode
+          };
+        }
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
